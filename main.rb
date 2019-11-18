5e3bcb32ba2907e73910f0374ea5f29e07977cca
ImBigman <b1gmanever@gmail.com>
2019-11-18 14:43:48 +0300

    add valid more than 21

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require './users.rb'
 require './card_deck.rb'
 
+# rubocop: disable Metrics/ClassLength
 class Game
   attr_reader :name, :players
   attr_reader :bank, :card_deck, :decks
@@ -23,18 +24,6 @@ class Game
     @players.push(Player.new(player_name), Dealer.new)
   end
 
-  def first_step
-    @decks = Cards.new
-    @decks.cards_price
-    deal_cards(@players[0], 2)
-    puts "Вам раздали карты, на руках #{@players[0].on_hand} "
-    puts "У вас #{count_cards(@players[0])} очков"
-    deal_cards(@players[1], 2)
-    puts 'У дилера на руках 2 карты [*] [*]'
-    take_bit
-    second_step
-  end
-
   def take_bit
     @players[0].money -= 10
     @players[1].money -= 10
@@ -43,10 +32,35 @@ class Game
   end
 
   def one_card
-    deal_cards(@players[0], 1)
-    puts "Вы взяли карту, на руках #{@players[0].on_hand}"
-    puts "У вас #{count_cards(@players[0])} очков"
-    dealers_step
+    if @players[0].on_hand.count == 3
+      puts 'У вас максимальное количество карт!'
+      second_step
+    elsif deal_cards(@players[0], 1)
+      puts "Вы взяли карту, на руках #{@players[0].on_hand}"
+      puts "У вас #{count_cards(@players[0])} очков"
+      second_step
+    end
+  end
+
+  def dealers_step
+    second_step
+    if @players[1].on_hand.count < 3 && count_cards(@players[1]) <= 17
+      deal_cards(@players[1], 1)
+      puts 'Диллер взял карту, на руках 3 карты [*] [*] [*]'
+    else
+      puts 'Диллер пропустил ход'
+    end
+    post_game
+  end
+
+  # rubocop: disable Metrics/AbcSize
+  def end_game
+    puts "У #{@players[0].name} на руках #{@players[0].on_hand}"
+    p count_cards(@players[0])
+    puts "У Диллера на руках #{@players[1].on_hand}"
+    p count_cards(@players[1])
+    valid
+    congratulations
   end
 
   def second_step
@@ -62,36 +76,43 @@ class Game
     end
   end
 
-  def dealers_step
-    if @players[1].on_hand.count < 3 && count_cards(@players[1]) <= 17
-      deal_cards(@players[1], 1)
-      puts 'Диллер взял карту, на руках 3 карты [*] [*] [*]'
-    else
-      puts 'Диллер пропустил ход'
-    end
+  def first_step
+    @decks = Cards.new
+    @decks.cards_price
+    deal_cards(@players[0], 2)
+    puts "Вам раздали карты, на руках #{@players[0].on_hand} "
+    puts "У вас #{count_cards(@players[0])} очков"
+    deal_cards(@players[1], 2)
+    puts 'У дилера на руках 2 карты [*] [*]'
+    take_bit
     second_step
-  end
-
-  def end_game
-    puts "У #{@players[0].name} на руках #{@players[0].on_hand}"
-    p count_cards(@players[0])
-    puts "У Диллера на руках #{@players[1].on_hand}"
-    p count_cards(@players[1])
-    congratulations
   end
 
   def congratulations
     if @players[0].on_hand_score <= 21 && @players[0].on_hand_score > @players[1].on_hand_score
-      puts "Победил #{@players[0].name}, у него на руках #{@players[0].money += @bank}$"
+      puts "Вы победили, на руках #{@players[0].money += @bank}$"
     elsif @players[1].on_hand_score <= 21 && @players[1].on_hand_score > @players[0].on_hand_score
       puts "Победил диллер, у него на руках #{@players[1].money += @bank}$"
     elsif @players[0].on_hand_score == @players[1].on_hand_score
       puts 'Ничья, все остались при своих деньгах.'
-      puts "#{@players[0].name}, на руках #{@players[0].money += @bank / 2}$"
-      puts "#{@players[1].name}, на руках #{@players[1].money += @bank / 2}$"
+      puts "#{@players[0].name}, на руках #{@players[0].money += (@bank / 2)}$"
+      puts "#{@players[1].name}, на руках #{@players[1].money += (@bank / 2)}$"
     end
-    post_game
   end
+
+  def valid
+    if @players[0].on_hand_score > 21
+      puts "Победил диллер, у него #{@players[1].money += @bank}$"
+      post_game
+    elsif  @players[1].on_hand_score > 21
+      puts "Вы победили, у вас #{@players[0].money += @bank}$"
+      post_game
+    elsif  @players[0].on_hand_score > 21 && @players[1].on_hand_score > 21
+      puts 'Вы оба проиграли!'
+      post_game
+    end
+  end
+  # rubocop: enable Metrics/AbcSize
 
   def deal_cards(player, quantity)
     @player = player
@@ -104,8 +125,8 @@ class Game
     summary ||= []
     @player = player
     cards = player.on_hand.map { |elem| elem[2..-1].to_sym }
-    cards[-1] = '1'.to_sym if cards.last == :A && cards.count(:A) == 2
     cards.each { |card| summary << decks.cards_parity[card] }
+    summary[-1] = 1 if summary.last == 11 && summary.count(11) == 2
     player.on_hand_score = summary.sum
   end
 
@@ -120,6 +141,7 @@ class Game
     choice == '1' ? first_step : exit
   end
 end
+# rubocop: enable Metrics/ClassLength
 
 def menu
   @game = Game.new
@@ -127,4 +149,4 @@ end
 menu
 @game.pre_game_menu
 @game.first_step
-
+@game.post_game
